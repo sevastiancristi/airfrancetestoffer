9a7a5796a2f153dc95d947bb068172e436bb968c
crist <crist@DESKTOP-A1N4BS0>
2022-02-15 10:03:37 +0200

    Updated database preload + postman collection

--- src/main/java/com/airfrance/testoffer/LoadDatabase.java
+++ src/main/java/com/airfrance/testoffer/LoadDatabase.java
@@ -1,9 +1,7 @@
 package com.airfrance.testoffer;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,17 +12,14 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
 	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-	@SuppressWarnings("deprecation")
 	@Bean
 	CommandLineRunner initDatabase(UserRepository repository) {
 
 		return args -> {
-//			log.info("Preloading " + repository
-//					.save(Mockito.any(User.class)));
 			log.info("Preloading " + repository
-					.save(new User("dsad", new SimpleDateFormat("yyyy-MM-dd").parse("2002-01-01"), "France",null,null)));
+					.save(new User("John Smith", new SimpleDateFormat("yyyy-MM-dd").parse("2002-01-01"), "France",null,null)));
 			log.info("Preloading " + repository
-					.save(new User("asewwqd", new SimpleDateFormat("yyyy-MM-dd").parse("2001-01-01"), "France",null,null)));
+					.save(new User("Vernon Nobody", new SimpleDateFormat("yyyy-MM-dd").parse("2001-01-01"), "France",null,User.Gender.MALE)));
 		};
 	}
 }
